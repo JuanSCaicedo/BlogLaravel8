5b2e3cab8ff193edd38ab457ea76e7efcacbcd67
JuanSCaicedo <sebanchit@gmail.com>
2024-04-22 21:28:36 -0500

    agregando rol published y validación desde servidor

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\PostRequest;
-use App\Models\Category;
+use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
 use App\Models\Tag;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests; // Asegúrate de importar AuthorizesRequests
 
-
+use App\Http\Requests\PostRequest;
 use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
@@ -18,15 +18,14 @@ class PostController extends Controller
     public function __construct()
     {
         $this->middleware('can:admin.posts.index')->only('index');
-        $this->middleware('can:admin.posts.create')->only('create','store');
-        $this->middleware('can:admin.posts.edit')->only('edit','update');
+        $this->middleware('can:admin.posts.create')->only('create', 'store');
+        $this->middleware('can:admin.posts.edit')->only('edit', 'update');
         $this->middleware('can:admin.posts.destroy')->only('destroy');
     }
 
+    use AuthorizesRequests;
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -37,109 +36,90 @@ class PostController extends Controller
     {
         return view('admin.posts.all');
     }
-    
+
     public function create()
     {
-        $categories = Category::pluck('name','id');
+        $categories = Category::pluck('name', 'id');
         $tags = Tag::all();
-        return view('admin.posts.create',compact('categories','tags'));
+
+        return view('admin.posts.create', compact('categories', 'tags'));
     }
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(PostRequest $request)
     {
+        // return Storage::put('posts', $request->file('file'));
         $post = Post::create($request->all());
 
-        if($request->file('file')){
-            $url =  Storage::put('posts',$request->file('file'));
+        if ($request->file('file')) {
+            $url = Storage::put('posts', $request->file('file'));
+
             $post->image()->create([
                 'url' => $url
             ]);
         }
 
-
-
-        if($request->tags){
+        if ($request->tags) {
             $post->tags()->attach($request->tags);
         }
 
-        Cache::flush();
-
-        return redirect()->route('admin.posts.edit',$post);
+        return redirect()->route('admin.posts.edit', $post);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Post $post)
     {
-        $this->authorize('author',$post);
+        $this->authorize('author', $post);
 
-        $categories = Category::pluck('name','id');
+        $categories = Category::pluck('name', 'id');
         $tags = Tag::all();
-        return view('admin.posts.edit',compact('post','categories','tags'));
+        $selectedTags = $post->tags->pluck('id')->toArray(); // Obtener los ID de las etiquetas del post
+        return view('admin.posts.edit', compact('post', 'categories', 'tags', 'selectedTags'));
     }
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
      */
     public function update(PostRequest $request, Post $post)
     {
-        $this->authorize('author',$post);
+        $this->authorize('author', $post);
 
         $post->update($request->all());
 
-        if($request->file('file')){
-            $url = Storage::put('posts',$request->file('file'));
+        if ($request->file('file')) {
+            $url = Storage::put('posts', $request->file('file'));
 
-            if($post->image){
+            if ($post->image) {
                 Storage::delete($post->image->url);
 
                 $post->image()->update([
                     'url' => $url
                 ]);
-            }else{
+            } else {
                 $post->image()->create([
                     'url' => $url
                 ]);
             }
-
         }
 
-        if($request->tags){
+        if ($request->tags) {
             $post->tags()->sync($request->tags);
         }
 
-        Cache::flush();
-
-        return redirect()->route('admin.posts.edit',$post)->with('info','El post se actualizó con éxito');
-
+        return redirect()->route('admin.posts.edit', $post)->with('info', 'Post Actualizado Correctamente');
     }
+
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
      */
     public function destroy(Post $post)
     {
-        $this->authorize('author',$post);
+        $this->authorize('author', $post);
 
         $post->delete();
-        Cache::flush();
-        return redirect()->route('admin.posts.index')->with('info','El post se elimino con éxito');
+
+        return redirect()->route('admin.posts.index', $post)->with('info', 'Post Eliminado Correctamente');
     }
 }

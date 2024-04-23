5ace183a7f1a180508e67333c920ab560bb3639a
JuanSCaicedo <sebanchit@gmail.com>
2024-04-23 16:54:17 -0500

    reponsive para tabla users y all posts

--- resources/views/livewire/admin/post-all.blade.php
+++ resources/views/livewire/admin/post-all.blade.php
@@ -4,53 +4,57 @@
         <div class="card-header">
             <input wire:model.live="search" class="form-control" placeholder="Ingrese el nombre de un post o autor" />
         </div>
-    
+
         @if ($posts->count())
             <div class="card-body">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nombre</th>
-                            <th>Autor</th>
-                            <th colspan="2"></th>
-                        </tr>
-                    </thead>
-    
-                    <tbody>
-                        @foreach ($posts as $post)
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
                             <tr>
-    
-                                <td class="text-sm d-sm-none">{{ $post->id }}</td>
-                                <!-- Texto más pequeño en dispositivos pequeños -->
-                                <td class="text-sm d-sm-none">{{ $post->name }}</td>
-                                <!-- Texto más pequeño en dispositivos pequeños -->
-                                <td class="d-none d-sm-table-cell">{{ $post->id }}</td>
-                                <!-- Mantiene el tamaño normal en dispositivos grandes -->
-                                <td class="d-none d-sm-table-cell">{{ $post->name }}</td>
-                                <!-- Mantiene el tamaño normal en dispositivos grandes -->
-    
-                                <!-- Mantiene el tamaño normal en dispositivos grandes -->
-                                <td class="d-none d-sm-table-cell"><span class="text-sm">{{ $post->user_id }} </span> :
-                                    {{ $post->user->name }}</td>
-    
-                                <td width="10px">
-                                    <a class="btn btn-primary btn-sm"
-                                        href="{{ route('admin.posts.edit', $post) }}">Editar</a>
-                                </td>
-                                <td width="10px">
-                                    <form action="{{ route('admin.posts.destroy', $post) }}" method="POST">
-                                        @csrf
-                                        @method('delete')
-                                        <button class="btn btn-danger btn-sm" type="submit">Eliminar</button>
-                                    </form>
-                                </td>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Autor</th>
+                                <th colspan="2"></th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+
+                        <tbody>
+                            @foreach ($posts as $post)
+                                <tr>
+                                    <td class="text-sm d-sm-none">{{ $post->id }}</td>
+                                    <!-- Texto más pequeño en dispositivos pequeños -->
+                                    <td class="text-sm d-sm-none">{{ $post->name }}</td>
+                                    <!-- Texto más pequeño en dispositivos pequeños -->
+                                    <td class="d-none d-sm-table-cell">{{ $post->id }}</td>
+                                    <!-- Mantiene el tamaño normal en dispositivos grandes -->
+                                    <td class="d-none d-sm-table-cell">{{ $post->name }}</td>
+                                    <!-- Mantiene el tamaño normal en dispositivos grandes -->
+                                    <td class="d-none d-sm-table-cell">
+                                        <span class="text-sm">{{ $post->user_id }}</span> :
+                                        {{ $post->user->name }}
+                                    </td>
+                                    <!-- Texto más pequeño en dispositivos pequeños -->
+                                    <td class="text-sm d-sm-none">
+                                        <span class="text-sm">{{ $post->user_id }}</span> :
+                                        {{ $post->user->name }}
+                                    </td>
+                                    <td width="10px">
+                                        <a class="btn btn-primary btn-sm" href="{{ route('admin.posts.edit', $post) }}">Editar</a>
+                                    </td>
+                                    <td width="10px">
+                                        <form action="{{ route('admin.posts.destroy', $post) }}" method="POST">
+                                            @csrf
+                                            @method('delete')
+                                            <button class="btn btn-danger btn-sm" type="submit">Eliminar</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-    
+
             <div class="card-footer">
                 {{ $posts->links() }}
             </div>
@@ -59,5 +63,5 @@
                 <strong>No hay ningún registro</strong>
             </div>
         @endif
-    </div>    
+    </div>
 </div>
--- resources/views/livewire/admin/users-index.blade.php
+++ resources/views/livewire/admin/users-index.blade.php
@@ -2,35 +2,37 @@
     <div class="card">
 
         <div class="card-header">
-            <input  wire:model="search" class="form-control" type="" placeholder="Ingrese el nombre o correo de un usuario">
+            <input wire:model="search" class="form-control" type="" placeholder="Ingrese el nombre o correo de un usuario">
         </div>
 
         @if($users->count())
         <div class="card-body">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>Email</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($users as $user)
-                    <tr>
-                        <td>{{ $user->id }}</td>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->email }}</td>
-                        <td width="10px ">
-                            @can('admin.users.edit')
-                                <a class="btn btn-primary" href="{{ route('admin.users.edit',$user) }}">Editar</a>
-                            @endcan
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td width="10px ">
+                                @can('admin.users.edit')
+                                    <a class="btn btn-primary" href="{{ route('admin.users.edit',$user) }}">Editar</a>
+                                @endcan
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
         <div class="card-footer">
             {{ $users->links() }}

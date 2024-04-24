b7da55b46584be65580633a31f2d2c033d6d143f
JuanSCaicedo <sebanchit@gmail.com>
2024-04-23 20:18:38 -0500

    fondo oscuro posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,11 @@
 <x-app-layout>
-
     <div class="container py-8">
-
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6">
             @foreach ($posts as $post)
-                <article class="w-full h-80 bg-cover bg-center @if ($loop->first) md:col-span-2 @endif"
-                    style="background-image: url(@if ($post->image) {{ Storage::url($post->image->url) }} @else {{ env('IMG_ALTERNATIVA') }} @endif)">
+                <article
+                    class="relative w-full h-80 bg-cover bg-center @if ($loop->first) md:col-span-2 @endif"
+                    style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(@if ($post->image) {{ Storage::url($post->image->url) }} @else {{ env('IMG_ALTERNATIVA') }} @endif)">
+
                     <div class="w-full h-full px-8 flex flex-col justify-center">
 
                         <div>
@@ -26,7 +25,6 @@
                     </div>
                 </article>
             @endforeach
-
         </div>
 
         <div class="mt-4">

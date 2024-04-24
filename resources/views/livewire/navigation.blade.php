2fbe94a537dbdfdb2554665d4ed10d56c7e7ddb0
JuanSCaicedo <sebanchit@gmail.com>
2024-04-23 20:26:51 -0500

    barra navegacion se pueda ver sobre imagen

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -71,7 +71,8 @@
                     </button>
 
                     <!-- Profile dropdown -->
-                    <div class="ml-3 relative" x-data="{ open: false }">
+                    <!-- Profile dropdown -->
+                    <div class="ml-3 relative z-10" x-data="{ open: false }">
                         <div>
                             <button x-on:click="open = true"
                                 class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
@@ -81,16 +82,7 @@
                                     alt="">
                             </button>
                         </div>
-                        <!--
-                                Profile dropdown panel, show/hide based on dropdown state.
-
-                                Entering: "transition ease-out duration-100"
-                                From: "transform opacity-0 scale-95"
-                                To: "transform opacity-100 scale-100"
-                                Leaving: "transition ease-in duration-75"
-                                From: "transform opacity-100 scale-100"
-                                To: "transform opacity-0 scale-95"
-                            -->
+                        <!-- Profile dropdown panel, show/hide based on dropdown state. -->
                         <div x-show="open" x-on:click.away="open = false"
                             class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
@@ -108,14 +100,13 @@
                                 @csrf
                                 <a href="{{ route('logout') }}"
                                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem"
-                                    onclick="event.preventDefault();
-                            this.closest('form').submit();">Cerrar
+                                    onclick="event.preventDefault(); this.closest('form').submit();">Cerrar
                                     sesión</a>
 
                             </form>
-
                         </div>
                     </div>
+
                 </div>
             @else
                 <div>
